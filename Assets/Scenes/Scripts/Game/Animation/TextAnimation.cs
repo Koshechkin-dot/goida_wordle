e91c824d9fcea118abf2637c5acb0ec3771b9347
Alex <iamkidandyouknowit@gmail.com>
2024-02-18 12:03:59 +0500

    Added basic animation, font and colors

---
+++ Assets/Scenes/Scripts/Game/Animation/TextAnimation.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using TMPro;
+
+public class TextAnimation : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI textMeshPro;
+    private string previousText = string.Empty;
+
+    private void Start()
+    {
+        
+        AnimateText();
+    }
+
+    private void Update()
+    {
+        if (textMeshPro.text != previousText)
+        {
+            AnimateText();
+            previousText = textMeshPro.text;
+        }
+    }
+
+    private void AnimateText()
+    {
+        LeanTween.value(gameObject, UpdateTextAnimation, 48f, 72f, 0.5f)
+            .setEase(LeanTweenType.easeOutElastic);
+    }
+
+    private void UpdateTextAnimation(float value)
+    {
+        textMeshPro.fontSize = value;
+    }
+}
