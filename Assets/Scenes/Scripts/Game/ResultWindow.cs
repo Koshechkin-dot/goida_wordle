2fab0ed1337ffd25c2b299858731db5f25b9a4d2
Alex <iamkidandyouknowit@gmail.com>
2024-03-17 09:49:22 +0500

    LeanTween animations on buttons added

--- Assets/Scenes/Scripts/Game/ResultWindow.cs
+++ Assets/Scenes/Scripts/Game/ResultWindow.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class ResultWindow : MonoBehaviour
@@ -7,6 +8,8 @@ public class ResultWindow : MonoBehaviour
     [SerializeField] private TextMeshProUGUI Score;
     [SerializeField] private TextMeshProUGUI Timer;
     [SerializeField] private TextMeshProUGUI SecretWord;
+    [SerializeField] private Image BackB;
+    [SerializeField] private Image NextWordB;
     private EventBus bus;
 
     public void Awake()
@@ -16,6 +19,7 @@ public class ResultWindow : MonoBehaviour
         bus = ServiceLocator.Instance.Get<EventBus>();
         bus.Subscribe<ResultShowEvent>(Activate);
     }
+
     private void OnDestroy()
     {
         bus.Unsubscribe<ResultShowEvent>(Activate);
@@ -35,9 +39,33 @@ public class ResultWindow : MonoBehaviour
         SceneManager.LoadScene("Menu");
     }
 
+    public void OnBackButtonPEnter()
+    {
+        LeanTween.scale(BackB.rectTransform, new Vector2(1.1f, 1.1f), 0.3f)
+            .setEase(LeanTweenType.easeOutQuad);
+    }
+
+    public void OnBackButtonPExit()
+    {
+        LeanTween.scale(BackB.rectTransform, new Vector2(1.0f, 1.0f), 0.3f)
+            .setEase(LeanTweenType.easeOutQuad);
+    }
+
     public void OnNextWordButtonClick()
     {
         gameObject.SetActive(false);
         bus.Invoke(new NextWordEvent());
+    }
+
+    public void OnNextWordButtonPEnter()
+    {
+        LeanTween.scale(NextWordB.rectTransform, new Vector2(1.1f, 1.1f), 0.3f)
+            .setEase(LeanTweenType.easeOutQuad);
+    }
+
+    public void OnNextWordButtonPExit()
+    {
+        LeanTween.scale(NextWordB.rectTransform, new Vector2(1.0f, 1.0f), 0.3f)
+            .setEase(LeanTweenType.easeOutQuad);
     }
 }
---
+++ Assets/Scenes/Scripts/Game/UI/Animation/MenuAnimation.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MenuAnimation : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI Title;
+    [SerializeField] private Image StartB;
+    [SerializeField] private Image ExitB;
+
+    public void OnTitlePDown()
+    {
+        LeanTween.scale(Title.rectTransform, new Vector2(1.3f, 1.3f), 0.3f)
+            .setEase(LeanTweenType.easeOutQuad);
+    }
+
+    public void OnTitlePUp()
+    {
+        LeanTween.scale(Title.rectTransform, new Vector2(1.0f, 1.0f), 0.3f)
+            .setEase(LeanTweenType.easeOutQuad);
+    }
+
+    public void OnStartButtonPEnter()
+    {
+        LeanTween.scale(StartB.rectTransform, new Vector2(1.1f, 1.1f), 0.3f)
+            .setEase(LeanTweenType.easeOutQuad);
+    }
+
+    public void OnStartButtonPExit()
+    {
+        LeanTween.scale(StartB.rectTransform, new Vector2(1.0f, 1.0f), 0.3f)
+            .setEase(LeanTweenType.easeOutQuad);
+    }
+
+    public void OnExitButtonPEnter()
+    {
+        LeanTween.scale(ExitB.rectTransform, new Vector2(1.1f, 1.1f), 0.3f)
+            .setEase(LeanTweenType.easeOutQuad);
+    }
+
+    public void OnExitButtonPExit()
+    {
+        LeanTween.scale(ExitB.rectTransform, new Vector2(1.0f, 1.0f), 0.3f)
+            .setEase(LeanTweenType.easeOutQuad);
+    }
+}
